3551a89a2799bc2b797d622f71d5c271adfa635c
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-03-13 17:50:57 +0530

    Callback Hell Or Pyramid of doom

--- JS/callbackHell.js
+++ JS/callbackHell.js
@@ -1,1 +1,12 @@
 //  Issues with callback: -> Callback hell -> inversion of control
+const Placeorder = function () {
+  const cart = function () {
+    const payment = function () {
+      const order = function () {
+        const orderId = function () {
+          console.log("Order placed successfully");
+        };
+      };
+    };
+  };
+};
